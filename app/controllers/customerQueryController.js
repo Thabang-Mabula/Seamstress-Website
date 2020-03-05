534ac5f6b0f6bf9175b61ea4a9f2855a688385ae
Thabang Mabula <1364103@students.wits.ac.za>
2020-03-05 20:13:05 +0200

    Extraced each part of the request

--- app/controllers/customerQueryController.js
+++ app/controllers/customerQueryController.js
@@ -1,8 +1,12 @@
 var sanitizeHtml = require('sanitize-html')
 
 function sendMail (queryObj) {
-  let queryContent = sanitizeHtml(queryObj.comment)
-  console.log(queryContent)
+  let name = sanitizeHtml(queryObj.name)
+  let email = sanitizeHtml(queryObj.emailAddress)
+  let contactNumber = sanitizeHtml(queryObj.contactNumber)
+  let comment = sanitizeHtml(queryObj.comment)
+
+  console.log(name + ' ' + email + ' ' + contactNumber + ' ' + comment)
 }
 
 module.exports = { sendMail: sendMail }
